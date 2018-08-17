0b9c1b1051b56d8713c2ead068c5e115c1971163
Aleksey Robul <alekseyrobul@gmail.com>
2018-08-17 15:25:29 +0300

    Update: Access BoomerangView fields in croutines

--- app/src/main/java/com/kotlin/alekseyrobul/boomerang/fragments/BoomerangFragment.kt
+++ app/src/main/java/com/kotlin/alekseyrobul/boomerang/fragments/BoomerangFragment.kt
@@ -7,11 +7,26 @@ import org.jetbrains.anko.support.v4.UI
 
 class BoomerangFragment: BaseFragment() {
 
+    /**
+     * Private fields
+     */
+    private var mViewListener = object : BoomerangFragmentViewListener {
+        override fun buttonTappedChooseVideoFile() {
+
+        }
+
+    }
+
+    /**
+     * Override funcs
+     */
     override fun updateUI(): View {
         return UI {
             constraintLayout {
                 val layout = constraintLayout()
-                boomerangView {}.lparams {
+                boomerangView(context = context) {
+                    viewListener = mViewListener
+                }.lparams {
                     topToTop = layout.top
                     bottomToBottom = layout.bottom
                     leftToLeft = layout.left
--- app/src/main/java/com/kotlin/alekseyrobul/boomerang/fragments/BoomerangFragmentView.kt
+++ app/src/main/java/com/kotlin/alekseyrobul/boomerang/fragments/BoomerangFragmentView.kt
@@ -3,24 +3,48 @@ package com.kotlin.alekseyrobul.boomerang.fragments
 import android.content.Context
 import android.view.View
 import android.view.ViewManager
+import com.kotlin.alekseyrobul.boomerang.R
 import com.kotlin.alekseyrobul.boomerang.views.VideoPlayer
 import com.kotlin.alekseyrobul.boomerang.views.videoSurfaceView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.constraint.layout.constraintLayout
 import org.jetbrains.anko.custom.ankoView
 
-inline fun ViewManager.boomerangView(theme: Int = 0, init: View.(demoView: BoomerangFragmentView) -> Unit): View {
-    val demoView = BoomerangFragmentView()
-    return ankoView({ demoView.createView(AnkoContext.create(it)) }, theme, { init(demoView) })
+/**
+ * Extensions
+ */
+inline fun ViewManager.boomerangView(theme: Int = 0, context: Context, init: BoomerangFragmentView.(view: BoomerangFragmentView) -> Unit): BoomerangFragmentView {
+    val bview = BoomerangFragmentView(context)
+    ankoView({ bview.createView(AnkoContext.create(it)) }, theme, { init(bview, bview) })
+    return bview
 }
 
-class BoomerangFragmentView: AnkoComponent<Context> {
+/**
+ * Listeners
+ */
+interface BoomerangFragmentViewListener {
+    fun buttonTappedChooseVideoFile()
+}
+
+class BoomerangFragmentView(context: Context): View(context), AnkoComponent<Context> {
+    /**
+     * Public fields
+     */
+    var viewListener:BoomerangFragmentViewListener? = null
+
+    /**
+     * Private fields
+     */
     private lateinit var mVideoPlayer: VideoPlayer
 
+    /**
+     * Override funcs
+     */
     override fun createView(ui: AnkoContext<Context>): View {
         return with(ui) {
             constraintLayout {
                 val layout = constraintLayout()
+
                 mVideoPlayer = videoSurfaceView(context = context) {
 
                 }.lparams(height = 800) {
@@ -29,7 +53,53 @@ class BoomerangFragmentView: AnkoComponent<Context> {
                     rightToRight = layout.right
                 }
 
+                button(text = R.string.button_save_video_file) {
+                    id = R.id.button_save_video_file
+                }.lparams {
+                    bottomToBottom = layout.bottom
+                    leftToLeft = layout.left
+                    rightToRight = layout.right
+                    bottomMargin = dip(16)
+                }
+
+                button(text = R.string.button_choose_video_file) {
+                    setOnClickListener { viewListener?.buttonTappedChooseVideoFile() }
+                }.lparams {
+                    bottomToTop = R.id.button_save_video_file
+                    leftToLeft = layout.left
+                    rightToRight = layout.right
+                }
             }
         }
     }
+
+    /**
+     * Public funcs
+     */
+    fun setVideoFile(path:String) {
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
